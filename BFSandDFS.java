bf8fc2f0535015f6ddbfa305f026b95a3b37c55a
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-04-10 17:30:24 +0530

    Create BFSandDFS.java

---
+++ BFSandDFS.java
@@ -0,0 +1,54 @@
+package StriverGraph;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+public class BFSandDFS {
+
+
+    public static void main(String args[]){
+
+    }
+
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean vis[] = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            bfs.add(node);
+
+            for(int i : adj.get(node)){
+                if(vis[i] == false){
+                    vis[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+        return bfs;
+    }
+
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        boolean vis[] = new boolean[V];
+        ArrayList<Integer> d = new ArrayList<>();
+        vis[0] = true;
+        dfs(0,vis,adj,d);
+        return d;
+    }
+    public static void dfs(int node, boolean vis[],ArrayList<ArrayList<Integer>> adj , ArrayList<Integer>d){
+        vis[node] = true;
+        d.add(node);
+         
+        for(int i : adj.get(node)){
+            if(vis[i] == false){
+               dfs(i,vis,adj,d);
+            }
+        }
+    }
+}
