124ac18dc18dc9d9119eea137be9f9236602538e
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-05-06 23:05:10 +0530

    Add files via upload

---
+++ DetectACycle.java
@@ -0,0 +1,75 @@
+package StriverGraph;
+import java.util.*;
+public class DetectACycle {
+    // public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        // int indegree[] = new int[V];
+        // for (int i = 0; i < V; i++) {
+        //     for (int j : adj.get(i)) {
+        //         indegree[j]++;
+        //     }
+        // }
+        // Queue<Integer> q = new LinkedList<Integer>();
+        // for (int i = 0; i < V; i++) {
+        //     if (indegree[i] == 0) {
+        //         q.add(i);
+        //     }
+        // }
+        //  int cnt = 0;
+        // // int i = 0;
+        // while (q.isEmpty() == false) {
+        //     int currNode = q.peek();
+        //     q.remove();
+        //     cnt++;
+        //     for (int j : adj.get(currNode)) {
+        //         indegree[j]--;
+        //         if (indegree[j] == 0) {
+        //             q.add(j);
+        //         }
+        //     }
+        // }
+    //    if(cnt == V) return false;
+    //    return true;
+
+
+    //    }
+
+    static int indegree[];
+    static int count = 0;
+
+    public static boolean detectCycle(int V, ArrayList<ArrayList<Integer>> adj){
+           countIndegree(V,adj);
+           
+
+    }
+
+    public static void countIndegree(int V , ArrayList<ArrayList<Integer>> adj){
+        indegree = new int[V];
+        for(int i = 0 ;i< V;i++){
+           for(int j : adj.get(i)){
+             indegree[j]++ ;
+           }
+        }
+    }
+    public static void bfs(int V , ArrayList<ArrayList<Integer>> adj){
+       Queue<Integer> q = new LinkedList<>();
+       for(int i = 0;i<V;i++){
+         if(indegree[i] == 0){
+            q.add(i);
+         }
+       }
+       int cnt = 0;
+       while(!q.isEmpty()){
+        int currentNode = q.peek();
+        q.remove();
+        cnt++;
+        for (int j : adj.get(currentNode)) {
+                    indegree[j]--;
+                    if (indegree[j] == 0) {
+                        q.add(j);
+                    }
+                }
+       }
+    }
+    }
+
