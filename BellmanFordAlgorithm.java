f16c69bd6e3f0d93bb066da66a3dd9a12b443555
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-06-12 17:49:41 +0530

    done on GFG

--- BellmanFordAlgorithm.java
+++ BellmanFordAlgorithm.java
@@ -20,6 +20,16 @@ public class BellmanFordAlgorithm {
                 }
             }
         }
+          for (ArrayList<Integer> it : edges) {
+            int u = it.get(0);
+            int v = it.get(1);
+            int wt = it.get(2);
+            if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                int temp[] = new int[1];
+                temp[0] = -1;
+                return temp;
+            }
+        }
         return dist;
     }
 }
