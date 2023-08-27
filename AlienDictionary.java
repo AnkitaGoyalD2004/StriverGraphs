a5a59d2962e2cb4a3ddbae89289b83d410b339de
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-08-27 23:55:14 +0530

    done

--- AlienDictionary.java
+++ AlienDictionary.java
@@ -6,7 +6,6 @@ public class AlienDictionar;
     // public static void main(String[] args){
 
     // }
-    
     public  static int indegree[];
     public static ArrayList<ArrayList<Integer>> getAdj(int K , String[] dic){
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
